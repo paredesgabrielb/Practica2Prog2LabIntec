0bb48e5aefc8fa6974f3e94d97db3ecad4051ac6
Gabriel Paredes <gparedes@digepres.gob.do>
2017-05-26 13:42:07 -0400

    Readme add

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Practica2Prog2LabIntec
+
+Tabla de frecuencia
+Cantidad x de estudio
+Cantidad y de muestras
+1. Cuantos estudios desea ingresar
+-Iniciando estudio 1
+--Cuantas muestras tiene el estudio?
+-10
+-Digite los 10 datos (tipo float)
+
+Va al menu y imprime mediana, moda, moda, desviacion standar, varianza
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <tchar.h>
+#include <windows.h>
+
+/**
+ * Practica:    No 2
+ * Materia:     Lab. Programacion 2
+ * Autor:       Gabriel Paredes
+ * Id:          1072445
+ * Horario:     Viernes 8-10pm
+ */
+
+// Practica 2 :
+// Tabla de frecuencia
+//  Cantidad x de estudio
+//  Cantidad y de muestras
+//  1. Cuantos estudios desea ingresar
+//  -Iniciando estudio 1
+//  --Cuantas muestras tiene el estudio?
+//  -10
+//  -Digite los 10 datos (tipo float)
+//  Va al menu y imprime mediana, moda, moda, desviacion standar, varianza
+//		sergio.lopez@gmail.com
+//		Titulo : LP_01_Matricula
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
